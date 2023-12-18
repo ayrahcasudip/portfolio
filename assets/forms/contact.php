873a80de58e9b329f5d850fd435c142af57c46c7
ayrahcasudip <144552930+ayrahcasudip@users.noreply.github.com>
2023-12-18 22:28:32 +0545

    Update contact.php

--- assets/forms/contact.php
+++ assets/forms/contact.php
@@ -1,27 +1,28 @@
 <?php
-  
-
-
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    $name = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
 
-    // You can perform additional validation and sanitization here
+    // Validate email
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Send an email with the form data
+        $to = "sudipacharya653@gmail.com"; // Replace with your email address
+        $subject = "Feedback from $name";
+        $messageBody = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
 
-    // Send an email with the form data
-    $to = "sudipacharya653@gmail.com"; // Replace with your email address
-    $subject = "Feedback from $name";
-    $messageBody = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
-
-    if (mail($to, $subject, $messageBody)) {
-        // Email sent successfully
-        echo "success"; // You can handle this response in your JavaScript
+        if (mail($to, $subject, $messageBody)) {
+            // Email sent successfully
+            echo "success"; // You can handle this response in your JavaScript
+        } else {
+            // Email sending failed
+            echo "error"; // You can handle this response in your JavaScript
+        }
     } else {
-        // Email sending failed
-        echo "error"; // You can handle this response in your JavaScript
+        // Invalid email format
+        echo "invalid-email"; // You can handle this response in your JavaScript
     }
 }
 ?>
+
